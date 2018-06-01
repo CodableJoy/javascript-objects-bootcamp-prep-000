1b15cff128e35fc3a9801f564206ab0623e08683
Joy Cheng <xuejiao.j.cheng@gmail.com>
2018-06-01 17:04:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 var playlist = {'Ed Sheeran': 'Perfect'};
 
 var updatePlaylist = function(playlist, artistName,songTitle){
+  var artist = artistName
   
- 
+  playlist[artist] = songTitle
+  return playlist;
 }
